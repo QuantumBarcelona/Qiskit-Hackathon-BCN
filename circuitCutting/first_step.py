65e18fdcb4f998a703983d0350ee6922fc83e930
AimbotParce <marcparcerisa@gmail.com>
2023-04-01 15:59:10 +0200

    Create main circuit in main program

--- circuitCutting/first_step.py
+++ circuitCutting/first_step.py
@@ -4,11 +4,19 @@ import numpy as np
 
 plt.rcParams.update({"font.size": 16})  # enlarge fonts
 
+from lib.step1 import *
 
 # Import standard qiskit modules
 from qiskit import QuantumCircuit, QuantumRegister
 
 # For doing exact simulation you can use Statevector (feel free to use something else)
 from qiskit.quantum_info import Statevector
+
+# %%
+cricFraction1.draw(output="mpl")
+# %%
+cricFraction2.draw(output="mpl")
+# %%
+mainCircuit.draw(output="mpl")
 
 # %%
