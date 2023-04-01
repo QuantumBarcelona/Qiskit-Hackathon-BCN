372e6be2271878d706ba0e2b8ea0fb10f9581978
JordiTorres01 <jtdurall01@gmail.com>
2023-04-01 18:30:47 +0200

    updated main simulation

--- circuitCutting/statevector_main.py
+++ circuitCutting/statevector_main.py
@@ -1,8 +1,9 @@
 #%%
 from lib.circuits.step1 import *
 from lib.measures import *
-from qiskit.quantum_info import Statevector
+from qiskit.quantum_info import Statevector, Operator
 import matplotlib.pyplot as mpl
+from lib import *
 #%%
 mainCirc = get_mainCirc()
 mainCirc.draw(output="mpl")
@@ -11,5 +12,12 @@ mainCirc.draw(output="mpl")
 statevector = Statevector.from_instruction(mainCirc)
 print(statevector)
 
+X = Operator.from_label("X")
+Result = statevector.expectation_value(X^X^X^X^X)
+print(Result)
 # %%
 mainCirc = Xmeasure(mainCirc,range(5),range(5))
+mainCirc.draw(output="mpl")
+# %%
+histogram(mainCirc,1000)
+# %%
