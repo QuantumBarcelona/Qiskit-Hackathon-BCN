d9d441291b650735248f56600e8a3f43da7932d6
bbobru <berta.bobru@gmail.com>
2023-04-01 16:29:55 +0200

    getCirc functions implemented

--- circuitCutting/lib/step1/total_circuit.py
+++ circuitCutting/lib/step1/total_circuit.py
@@ -14,17 +14,18 @@ from qiskit.quantum_info import Statevector
 # %%
 # MAIN CIRCUIT
 
-circ = QuantumCircuit(5, 5)
-circ.h(range(5))
-circ.cx(0, 4)
-circ.rx(np.pi / 2, 4)
-circ.cx(0, 3)
-circ.rx(np.pi / 2, 0)
-circ.rx(np.pi / 2, 1)
-circ.rx(np.pi / 5, 3)
-circ.cx(3, 1)
-circ.rx(np.pi / 2, 1)
-circ.cx(2, 3)
-circ.rx(np.pi / 2, 2)
-
+def get_mainCirc():
+    circ = QuantumCircuit(5, 5)
+    circ.h(range(5))
+    circ.cx(0, 4)
+    circ.rx(np.pi / 2, 4)
+    circ.cx(0, 3)
+    circ.rx(np.pi / 2, 0)
+    circ.rx(np.pi / 2, 1)
+    circ.rx(np.pi / 5, 3)
+    circ.cx(3, 1)
+    circ.rx(np.pi / 2, 1)
+    circ.cx(2, 3)
+    circ.rx(np.pi / 2, 2)
+    return circ
 # %%
