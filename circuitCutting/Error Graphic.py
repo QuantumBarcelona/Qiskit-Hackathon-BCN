42ab04bd1ed954770f893cbc9bd39972c576d47d
JordiTorres01 <jtdurall01@gmail.com>
2023-04-01 22:41:56 +0200

    Millores

--- circuitCutting/Error Graphic.py
+++ circuitCutting/Error Graphic.py
@@ -4,25 +4,30 @@ import numpy as np
 
 def expected_X_full_ciruit(n):
     expected_X_full_ciruit = n**2
-    return
+    return expected_X_full_ciruit
 
 def expected_X_split_ciruit(n):
     expected_X_split_ciruit = n**3
-    return
+    return expected_X_split_ciruit
 
-full_circuit_error = np.ndarray(1000,dtype=float)
-split_circuit_error = np.ndarray(1000,dtype=float)
-N = range(100,10000,1)
+
+N = list(range(100,1000,100))
+full_circuit_error = np.ndarray(len(N),dtype=float)
+split_circuit_error = np.ndarray(len(N),dtype=float)
 
 full_circuit_error = [expected_X_full_ciruit(n) for n in N]
 split_circuit_error = [expected_X_split_ciruit(n) for n in N]
 
+print(full_circuit_error)
+print(split_circuit_error)
+
 fig, ax = plt.subplots()
 ax.plot(N,full_circuit_error)
 ax.plot(N,split_circuit_error)
-# ax.xax(N[0],N[-1])
-# ax.yax(expected_X_split_ciruit[0],expected_X_split_ciruit[-1])
 
-# ax.set_yscale('log')
+ax.set_yscale('log')
+ax.set_xscale('log')
+
 plt.show()
+
 # %%
