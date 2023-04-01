9e3a30fae9a5cf326b0231a7e1bcdffd9923efe6
JordiTorres01 <jtdurall01@gmail.com>
2023-04-01 18:06:46 +0200

    hola

--- circuitCutting/statevector_main.py
+++ circuitCutting/statevector_main.py
@@ -1,10 +1,11 @@
 #%%
-from lib.step1 import *
+from lib.circuits.step1 import *
 from lib.measures import *
 from qiskit.quantum_info import Statevector
 import matplotlib.pyplot as mpl
 
-# %%
+# %%0
+mainCircuit = get_mainCirc()
 mainCirc = Xmeasure(mainCircuit,range(3),range(3))
 mainCirc.draw(output="mpl")
 # %%
