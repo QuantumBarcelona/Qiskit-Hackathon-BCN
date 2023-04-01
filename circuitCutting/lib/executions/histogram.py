0ad72d8fb737a01ba11cf5ffbe75a3b2773f9f0f
AimbotParce <marcparcerisa@gmail.com>
2023-04-01 19:46:00 +0200

    Added complete statevector computation

--- circuitCutting/lib/executions/histogram.py
+++ circuitCutting/lib/executions/histogram.py
@@ -1,15 +1,15 @@
 # %%
 
-from qiskit import QuantumCircuit, Aer, execute
+from qiskit import Aer, QuantumCircuit, execute
 from qiskit.tools.visualization import plot_histogram
-
 
 # %%
 # SIMULATOR WITH HISTOGRAM
+
 
 def histogram(circ, s):
     backend = Aer.get_backend("qasm_simulator")
     job = execute(circ, backend, shots=s)
     result = job.result()
     counts = result.get_counts(circ)
-    return plot_histogram(counts), counts
+    return counts
--- circuitCutting/statevector_main.py
+++ circuitCutting/statevector_main.py
@@ -1,9 +1,10 @@
 #%%
-from lib.circuits.step1 import *
-from lib.measures import *
-from qiskit.quantum_info import Statevector, Operator
 import matplotlib.pyplot as mpl
+from lib.circuits.step1 import *
 from lib.executions import *
+from lib.measures import *
+from qiskit.quantum_info import Operator, Statevector
+
 #%%
 mainCirc = get_mainCirc()
 mainCirc.draw(output="mpl")
@@ -13,11 +14,22 @@ statevector = Statevector.from_instruction(mainCirc)
 print(statevector)
 
 X = Operator.from_label("X")
-Result = statevector.expectation_value(X^X^X^X^X)
+print("X:", X)
+Result = statevector.expectation_value(X ^ X ^ X ^ X ^ X)
 print(Result)
 # %%
-mainCircuit = Xmeasure(mainCirc,range(5),range(5))
+mainCircuit = Xmeasure(mainCirc, range(5), range(5))
 mainCircuit.draw(output="mpl")
 # %%
-histogram(mainCircuit,1000)
+count = histogram(mainCircuit, 1000)
+
 # %%
+Z = Operator.from_label("Z")
+# Compute expectation value:
+expectation = 0
+totalTimes = sum(count.values())
+for key, times in count.items():
+    state = Statevector.from_label(key)
+    expectation += state.expectation_value(Z ^ Z ^ Z ^ Z ^ Z) * times / totalTimes
+
+print("Expectation value:", expectation)
