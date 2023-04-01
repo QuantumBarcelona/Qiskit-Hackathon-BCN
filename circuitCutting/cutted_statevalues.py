33ff7eb25f10f57e596b4553a5b6be5036331323
FerranRMascaro <ferran.r.m11@gmail.com>
2023-04-01 18:36:30 +0200

    Cutted SV

--- circuitCutting/cutted_statevalues.py
+++ circuitCutting/cutted_statevalues.py
@@ -8,19 +8,21 @@ from qiskit.quantum_info import Statevector
 # %%
 # SECOND CIRCUIT STATEVECTORS
 
-def cutted_statevalues(circ,cq):
+def cutted_statevalues(circ,cq,op):
     # Creation of different cq states circuits:
     qnum = circ.num_qubits
     expVal = []
     for c in ["0","1","+","-","r","l","0","1"]:
         label = "0"*(cq-1) + c + "0"*(qnum-cq)
         stateVector = Statevector.from_label(label)
-        expVal.append(stateVector.expectation_value(circ))
+        expVal.append(stateVector.expectation_value(op))
     return expVal
 
 
 
 # %%
 from lib.circuits.step1 import *
+from qiskit.quantum_info.operators import Operator
+X = Operator.from_label("X")
 circ = get_circ2()
-expVal = cutted_statevalues(circ,0)
+expVal = cutted_statevalues(circ,0,X)
