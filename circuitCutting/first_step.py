fb3e09701f8f2f62ede346b094db9a0bd3251437
AimbotParce <marcparcerisa@gmail.com>
2023-04-01 16:35:39 +0200

    Add histogram

--- circuitCutting/first_step.py
+++ circuitCutting/first_step.py
@@ -4,11 +4,12 @@ import numpy as np
 
 plt.rcParams.update({"font.size": 16})  # enlarge fonts
 
+from lib.histogram import histogram
 from lib.measures import Xmeasure, Ymeasure
 from lib.step1 import *
 
 # Import standard qiskit modules
-from qiskit import QuantumCircuit, QuantumRegister
+from qiskit import QuantumCircuit, QuantumRegister, execute
 
 # For doing exact simulation you can use Statevector (feel free to use something else)
 from qiskit.quantum_info import Statevector
@@ -26,13 +27,18 @@ cricFraction1 = get_circ1()
 # Measure the cubit 1 from the first circuit
 cricFraction1.measure(1, 0)
 cricFraction1.draw(output="mpl")
+histogram(cricFraction1)
+
 # %%
 cricFraction1 = get_circ1()
 Xmeasure(cricFraction1, 1, 1)
 cricFraction1.draw(output="mpl")
+
+histogram(cricFraction1)
 # %%
 cricFraction1 = get_circ1()
 Ymeasure(cricFraction1, 1, 2)
 cricFraction1.draw(output="mpl")
 
+histogram(cricFraction1)
 # %%
