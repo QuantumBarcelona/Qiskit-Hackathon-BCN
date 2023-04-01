7eeed91d731bf92ea060452760ff5340f980fb85
FerranRMascaro <ferran.r.m11@gmail.com>
2023-04-01 17:53:54 +0200

    Begining cutted statevalues

---
+++ circuitCutting/lib/step1/cutted_statevalues.py
@@ -0,0 +1,19 @@
+# %%
+# SECOND CIRCUIT STATEVECTORS# Import standard qiskit modules
+from qiskit import QuantumCircuit, QuantumRegister
+# For doing exact simulation you can use Statevector (feel free to use something else)
+from qiskit.quantum_info import Statevector
+
+
+# %%
+# SECOND CIRCUIT STATEVECTORS
+
+# Circuit:
+circ = 
+
+# Qubit cutted:
+cq = 
+
+# Creation of different cq states circuits:
+    qnum = QuantumCircuit.num_qubits(circ)
+    
