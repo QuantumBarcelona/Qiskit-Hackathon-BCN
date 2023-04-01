4aec48cca3fb9e78af2438f5c3780397e34ee5e9
FerranRMascaro <ferran.r.m11@gmail.com>
2023-04-01 16:35:47 +0200

    Histogram

--- circuitCutting/lib/histogram.py
+++ circuitCutting/lib/histogram.py
@@ -7,9 +7,9 @@ from qiskit.tools.visualization import plot_histogram
 # %%
 # SIMULATOR WITH HISTOGRAM
 
-def histogram(circ):
+def histogram(circ, shots):
     backend = Aer.get_backend("qasm_simulator")
-    job = execute(circ, backend, shots=10000)
+    job = execute(circ, backend, shots)
     result = job.result()
     counts = result.get_counts(circ)
     plot_histogram(counts)
