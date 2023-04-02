4860303c89d1dcf7aba472619d7c82ca8e443591
AimbotParce <marcparcerisa@gmail.com>
2023-04-02 09:43:56 +0200

    Create download data

---
+++ circuitCutting/simulation/download_data.py
@@ -0,0 +1,41 @@
+# %%
+import os
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+plt.rcParams.update({"font.size": 16})  # enlarge fonts
+
+import sys
+
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
+import json
+from concurrent.futures import ThreadPoolExecutor
+
+from lib.circuits.step1 import *
+from lib.measures import measure_from_label
+from qiskit import QuantumCircuit, transpile
+from qiskit.quantum_info import Statevector
+from qiskit.quantum_info.operators import Operator
+from qiskit_ibm_provider import IBMProvider
+
+provider = IBMProvider(os.environ["IBMQ_TOKEN"])
+
+hub = "ibm-q-community"
+group = "digiq-icfo-hack"
+project = "main"
+
+backend_name = "ibmq_jakarta"  # 7 qubits
+
+# %%
+jobs = provider.backend.jobs(backend_name=backend_name, limit=50, status="DONE")
+print(f"Found {len(jobs)} jobs")
+
+# %%
+for job in jobs:
+    print(job.job_id())
+    res = job.result()
+
+    print(res)
+    print(res.get_counts())
+# %%
